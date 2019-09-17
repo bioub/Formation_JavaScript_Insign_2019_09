1621b6589c258cd124b641503a6e772f55761eac
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2019-09-17 13:59:21 +0200

    Correction

--- Exercice-Build/build.js
+++ Exercice-Build/build.js
@@ -12,3 +12,22 @@ const indexHtmlPath = path.resolve(srcPath, 'index.html');
 const indexHtmlDistPath = path.resolve(distPath, 'index.html');
 const appJsDistPath = path.resolve(distPath, 'app.js');
 
+async function build() {
+  await fs.remove(distPath);
+  await fs.mkdir(distPath);
+
+  const contents = await Promise.all([
+    fs.readFile(horlogeJsPath),
+    fs.readFile(indexJsPath),
+  ]);
+
+  await fs.writeFile(appJsDistPath, Buffer.concat(contents));
+
+  let contentIndexHtml = await fs.readFile(indexHtmlPath, { encoding: 'UTF-8' });
+  contentIndexHtml = contentIndexHtml.replace(/<script.*<\/script>/s, '<script src="./app.js"></script>');
+
+  await fs.writeFile(indexHtmlDistPath, contentIndexHtml);
+  console.log('Build done');
+}
+
+build();
